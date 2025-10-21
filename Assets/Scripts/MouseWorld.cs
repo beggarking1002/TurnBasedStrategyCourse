8bfedf0b8337adddfab810288e76de22dca60c4e
Beggarking1002 <drake1002@naver.com>
2025-10-21 13:51:45 +0900

    Mouse Raycast
    Mouse Raycast

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.Log(Physics.Raycast(ray));
+    }
+}
