a345cf8bd9f87071de0a031579b4bb8e22a1b908
Beggarking1002 <drake1002@naver.com>
2025-10-21 15:01:13 +0900

    Mouse World Position
    Mouse World Position

--- Assets/Scripts/MouseWorld.cs
+++ Assets/Scripts/MouseWorld.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class MouseWorld : MonoBehaviour
 {
-    private void Update()
+    private static MouseWorld instance;
+
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static Vector3 GetPosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.Log(Physics.Raycast(ray));
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLayerMask);
+        return raycastHit.point;
+
     }
 }
