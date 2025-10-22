3ea2137cbdccf0dc78b4b9627630845e479c0c6f
Beggarking1002 <drake1002@naver.com>
2025-10-23 07:56:17 +0900

    Unit Rotate/Unit Selection
    Unit Rotate/Unit Selection

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,6 +6,11 @@ public class Unit : MonoBehaviour
 
     private Vector3 targetPosition;
 
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
     private void Update()
     {
         float stoppingDistance = .1f;
@@ -14,19 +19,19 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
             unitAnimator.SetBool("IsWalking", true);
         }
         else
         {
             unitAnimator.SetBool("IsWalking", false);
         }
-        if (Input.GetMouseButtonDown(0))
-        {
-            Move(MouseWorld.GetPosition());
-        }
+
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
